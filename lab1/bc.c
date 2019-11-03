7c1af53ef788469ed8bb189069c25f844cef42d8
RoyRyy <2524380603@qq.com>
2019-11-03 22:06:31 +0800

    added a bc.c file

--- lab1/bc.c
+++ lab1/bc.c
@@ -1,16 +1,24 @@
-#include<stdio.h>
-int main(){
-    long long input;
-    int lb=0;
-    scanf ("%lld",&input);
-    while(input){
-        if(input %2==1){
-            lb=lb+1;
-        }
-        input /=2;
-    }
-    printf("%d\n",lb);
-    return 0;
-
-
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("输入一个十进制数:");
+   
+   int n;
+   int c[64],i=0;
+   scanf("%d",&n);
+   do{
+       c[i++]=n%2;
+       n=n/2;
+   }while(n!=0);
+   printf("对应的十进制数是：");
+   
+  int a=0;
+   for (i--;i>=0;i--){
+    if (c[i]>0)
+    a++;
+    printf("%d",c[i]);}
+    
+    printf("\n这个二进制数中1的个数：");
+    printf("%d\n",a);
 }
