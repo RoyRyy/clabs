21ef35cce37c75566775ead46f7acb289fa48144
RoyRyy <2524380603@qq.com>
2019-12-18 22:03:26 +0800

    added a ex2.c file

--- lab5/ex2.c
+++ lab5/ex2.c
@@ -1,14 +1,69 @@
-#include<stdio.h>
-float add(float x,float y)
+#include <stdio.h>
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+main()
 {
-    float z;
-    z=x+y;
-    return z;
+    float a[10],*p,j,m=0;
+    int i,o;
+    p=a;
+    printf("请输入你要求和的浮点数个数：");
+    scanf("%d",&o);
+    printf("请输入这些浮点数，以回车键结束：\n");
+    for (i=0;i<o;i++){
+     getfloat(&p[i]);}
+    for (i=0;i<o;i++)
+    {
+      
+       j=p[i];
+       m=m+j;
+    }
+    printf("the sum is:");
+    printf("%1.2f",m);
 }
-int main(){
-float a,b,c;
-printf("please input a and b:\n");
-scanf("%f %f",&a,&b);
-c=add(a,b);
-printf("the sum is:%1.3f",c);
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp=0;
+int getch(void)
+{
+    return (bufp>0)? buf[--bufp]:getchar();
+}
+void ungetch(int c)
+{
+    if (bufp>=BUFSIZE)
+       printf("ungetch:too many characters\n");
+    else
+    {
+         buf[bufp++]=c ;
+    }
+    
+}
+int getfloat (float *pn)
+{
+    int c,sign;
+    float power;
+    while (isspace(c=getch()))
+    ;
+    if (!isdigit(c) && c!=EOF && c!= '+' && c !='-'&& c!='.'){
+        ungetch(c);
+        return 0;
+    }
+    sign = (c =='-')? -1:1;
+    if (c=='+'||c=='-')
+        c=getch();
+    for (*pn=0.0;isdigit(c);c=getch())
+       *pn=10.0 * *pn+(c-'0');
+    if (c=='.')
+        c=getch();
+    for (power=1.0;isdigit(c);c=getch()){
+        *pn=10.0 * *pn+(c-'0');
+        power *=10.0;
+    }
+    *pn *=sign/power;
+    if (c!=EOF)
+      ungetch(c);
+    return c;
+
 }
