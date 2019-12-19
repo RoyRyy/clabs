70871d627e5050e86a9a37c3b85e4d992caec128
RoyRyy <2524380603@qq.com>
2019-12-19 20:29:38 +0800

    added a ex1.c file

--- lab3/ex1.c
+++ lab3/ex1.c
@@ -1,35 +1,25 @@
 #include<stdio.h>
-#include<string.h>
-int count(char str[]){
-    int i = 0;
-    while(str[i++] !='\0'){
-        ;
-    }
-    i--;i--;
-    return i;
-}
-int strrindex(char pattern[],char line[]){
-    int a = count(line);
-    int b = count(pattern);
-    
-    int i = a;
-    while(i >=0){
-      int k = 0;int j = i - b;
-      while(j >=0 && line[j] == pattern[k]){
-      if(k == b){
-          i = i - b;
-          return i;
-         }
-      k++;j++;
-      }
-      i--;
-    }
-      return -1;
-}
-int main()
+#include<string.h>  //运用函数库函数strlen,用来计算字符串长度。
+main()
 {
-    char line[1000];
-    char pattern[1000];
-    scanf("%s %s",pattern,line);
-    printf("%d\n",strrindex(pattern,line));
+    char a[10],b[10];
+    int i;
+    printf("输入两个字符串，中间以空格间隔：");
+    scanf("%s %s",&a,&b);
+    i=strrindex(a,b);
+    printf("%d\n",i);
+
+}
+
+
+int strrindex (char s[],char t[])
+{
+    int m,u,k;
+    for (m=strlen(s)-strlen(t);m>=0;m--){
+        for (u=m,k=0;t[k]!='\0' && s[u]==t[k];u++,k++)
+          ;
+        if (k>0 && t[k]=='\0')
+          return m;
+    }
+     return -1;
 }
