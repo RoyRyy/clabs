663eb0af872a90ec1c0cb26a45691dbeda94260f
RoyRyy <2524380603@qq.com>
2019-12-19 22:06:53 +0800

    added a ex1.c file

--- lab3/ex1.c
+++ lab3/ex1.c
@@ -1,25 +1,28 @@
-#include<stdio.h>
-#include<string.h>  //运用函数库函数strlen,用来计算字符串长度。
-main()
+#include <stdio.h>
+main (int argc,char *argv[])
 {
-    char a[10],b[10];
     int i;
-    printf("输入两个字符串，中间以空格间隔：");
-    scanf("%s %s",&a,&b);
-    i=strrindex(a,b);
+    i=strrindex(argv[1],argv[2]);
     printf("%d\n",i);
-
 }
-
-
 int strrindex (char s[],char t[])
 {
-    int m,u,k;
-    for (m=strlen(s)-strlen(t);m>=0;m--){
-        for (u=m,k=0;t[k]!='\0' && s[u]==t[k];u++,k++)
+    int r,u,k;
+    int i=0,j=0;
+    int m;  
+    while(s[i]!='\0'){
+    i++;}
+    while(t[j]!='\0'){
+    j++;}
+    if (i>=j)
+     m=i-j;
+    else 
+     m=j-i;
+    for (r=m;r>=0;r--){
+        for (u=r,k=0;t[k]!='\0' && s[u]==t[k];u++,k++)
           ;
         if (k>0 && t[k]=='\0')
-          return m;
+          return r;
     }
      return -1;
 }
