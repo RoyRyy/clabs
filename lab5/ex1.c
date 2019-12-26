e50a815f125fe60e23209d7bd40e3b82db276b44
RoyRyy <2524380603@qq.com>
2019-12-26 21:18:37 +0800

    added a ex1.c file

--- lab5/ex1.c
+++ lab5/ex1.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
-void reverse(char *m){
-	char a =*m;
-    
-	if(a ){
-		reverse(m=m+1);
-      
-	}
-    printf("%c",a);
+void reverse(char s[]){
+   int c,j,i=0,k=0;
+   while(s[k]!='\0'){
+    k++;}
+   j=k-(i+1);
+   for(i=0;i<j;i++,j--)
+     {
+     if(i<j)
+    c=s[i];
+    s[i]=s[j];
+    s[j]=c;}
 }
 int main(){
     printf("请输入一个字符串我来倒置：");
 	char *s;
-    scanf("%s",*&s);
-    
+    scanf("%s",*&s);  
 	reverse(s);
-    
+    printf("%s",s);
 	return 0 ;
 }
