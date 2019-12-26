98ffa1adba423d16066a3db480a7ef16f9cebe44
RoyRyy <2524380603@qq.com>
2019-12-26 21:56:40 +0800

    added a ex1.c file

--- lab3/ex1.c
+++ lab3/ex1.c
@@ -1,28 +1,36 @@
-#include <stdio.h>
-main (int argc,char *argv[])
+#include<stdio.h>
+__INT16_TYPE__ stringminus(char a[],char b[])
 {
-    int i;
-    i=strrindex(argv[1],argv[2]);
-    printf("%d\n",i);
-}
-int strrindex (char s[],char t[])
-{
-    int r,u,k;
     int i=0,j=0;
-    int m;  
-    while(s[i]!='\0'){
+    int m; 
+    while(a[i]!='\0'){
     i++;}
-    while(t[j]!='\0'){
+    while(b[j]!='\0'){
     j++;}
     if (i>=j)
      m=i-j;
     else 
      m=j-i;
-    for (r=m;r>=0;r--){
+    return m;
+}
+int strrindex (char s[],char t[])
+{
+    int r,u,k;
+    
+    for (r=stringminus(s,t);r>=0;r--){
         for (u=r,k=0;t[k]!='\0' && s[u]==t[k];u++,k++)
           ;
         if (k>0 && t[k]=='\0')
           return r;
     }
      return -1;
+}
+int main()
+{
+    char f[100],j[100];
+    int i;
+    printf("输入两个字符串，中间以空格间隔：");
+    scanf("%s %s",f,j);
+    i=strrindex(f,j);
+    printf("%d\n",i);
 }
